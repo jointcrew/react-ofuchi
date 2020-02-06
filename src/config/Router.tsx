11a881ef71405530b8a6d9d16cc1bbc8f29105bb
oh-ryo <oh.ryo030205@gmail.com>
2020-02-06 18:34:30 +0900

    refactor:Response to omission of comment out code

--- src/config/Router.tsx
+++ src/config/Router.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-//import {GalleryList} from '../components/organisms/Gallerylist';
 import {routePath} from '../constants/Route';
 import Main from '../components/pages/Main';
 import List from '../components/pages/List';
@@ -11,7 +10,6 @@ export const Routing: React.FC = () => {
   return(
     <Router>
       <Switch>
-
         <Route path={routePath.PAGE_ONE}>
           <List url={0} />
         </Route>
