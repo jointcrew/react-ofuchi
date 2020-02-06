6937fa351b0507d5319167c21140fe943221de7f
oh-ryo <oh.ryo030205@gmail.com>
2020-02-06 16:08:14 +0900

    refactor: Delete comment out code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-//import Header from './components/organisms/header';
-//import Content from './content';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Main from './components/pages/main';
 import List from './components/pages/list';
