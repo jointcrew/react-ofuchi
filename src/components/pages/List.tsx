aa0ba2a58853de22c2ab972c9a91eb2bd268bbda
oh-ryo <oh.ryo030205@gmail.com>
2020-02-17 13:30:02 +0900

    refactor:Change type definition and Name change of map function

--- src/components/pages/List.tsx
+++ src/components/pages/List.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
+import * as H from 'history'
+import {RouteComponentProps} from 'react-router-dom'
 import {GalleryList} from '../organisms/Gallerylist';
 
-interface ListNum {
-  listNum: number
+interface ListProps extends RouteComponentProps{
+  location: H.Location<{listNum: number}>
 }
 
-interface StateList {
-  state: ListNum
-}
-
-interface ListNumProps{
-  location: StateList
-}
-
-const List: React.FC<ListNumProps> = (props) => {
+const List = (props: ListProps) => {
   return(
     <div>
       <GalleryList listNum={props.location.state.listNum} />
