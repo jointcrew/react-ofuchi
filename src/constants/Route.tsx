c2f630cbc790858c6cba85be6dbcff9dd87845af
oh-ryo <oh.ryo030205@gmail.com>
2020-02-25 19:00:33 +0900

    refactor:Description of operation comment and modification of RouteURL

--- src/constants/Route.tsx
+++ src/constants/Route.tsx
@@ -2,5 +2,6 @@ export const routePath = {
   PAGE_TOP : "/",
   GALLERY : "/gallery",
   LOGIN: "/login",
-  PASSWORD_RESET: "/password/reset",
+  PASSWORD_RESET: "/password-reset",
 }
+// ルーティング時のURLパス設定
