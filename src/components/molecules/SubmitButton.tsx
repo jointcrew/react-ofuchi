e73ddaf403736a5dca66b8bfd3eb1b9d3e569cc6
oh-ryo <oh.ryo030205@gmail.com>
2020-02-27 11:32:52 +0900

    refactor: Corrected an error in the operation comment

--- src/components/molecules/SubmitButton.tsx
+++ src/components/molecules/SubmitButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 // reactのコードを機能させるために必要なコンポーネントの読み込み
 import { Button } from 'antd';
-// antdesignのForm、Buttonコンポーネントの読み込み
+// antdesignのButtonコンポーネントの読み込み
 
 interface ButtonProps {
   error: (names?: string[] | undefined) => Record<string, string[] | undefined>
-  // 関数型として型定義（引数nameに値が入っている場合はstring型の配列もしくはundefined型、returnのstring型をstring型の配列もしくはundefined型として型定義）
+  // 関数型として型定義（引数nameに値が入っている場合はstring型の配列もしくはundefined型、returnのstring型の配列もしくはundefined型をstring型として型定義）
   buttonLabel: string
   // 親コンポーネントから受け取るpropsをstring型として型定義
   // propsの型定義
@@ -16,7 +16,7 @@ interface ButtonProps {
 const SubmitButton: React.FC<ButtonProps> = (props) => {
   // 関数コンポーネントかつButtonPropsとして型定義を行いpropsを受け取る
   const hasErrors = (fieldsError: { [x: string]: unknown; }): boolean => {
-    // 引数fieldsErrorを型ガードを使用してunknown型、returnをboolean型として型定義
+    // fieldsErrorにあるstring型の引数xをunknown型、returnをboolean型として型定義
     return Object.keys(fieldsError).some(field => fieldsError[field]);
   }
   // fieldsErrorオブジェクトの名前部分を配列に変換して配列項目にエラーが出るかチェック
