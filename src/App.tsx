7f4cbb190e444aa6807fd7843acb99bc3bc587b7
oh-ryo <oh.ryo030205@gmail.com>
2020-02-28 19:06:07 +0900

    refactor:Header component routing setting change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 import 'antd/dist/antd.css';
 import {Router} from './config/Router';
-import Header from './components/organisms/Header';
 
 
 
@@ -11,7 +10,6 @@ import Header from './components/organisms/Header';
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Router />
     </BrowserRouter>
   );
