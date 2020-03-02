f37fb9da29fd86cc416de41461866d846497326b
oh-ryo <oh.ryo030205@gmail.com>
2020-03-02 15:53:37 +0900

    refactor:Initialize login page

--- src/config/Router.tsx
+++ src/config/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // reactのコードを機能させるために必要なコンポーネントの読み込み
-import {Route} from "react-router-dom";
+import {Route, Redirect} from "react-router-dom";
 // 各コンポーネントへのルーティング設定に必要なコンポーネントの読み込み
 import {routePath} from '../constants/Route';
 // 各コンポーネントへのルーティング用URLパスの読み込み
@@ -19,20 +19,25 @@ export const Router: React.FC = (): JSX.Element => {
   // 関数コンポーネントをreact側で定義しているReact.FC型、returnをreact側で定義しているJSX.Element型で型定義を行い他のコンポーネントでも関数を使用できるようにエクスポート
   return(
     <>
-        <Route exact path={[
-          routePath.PAGE_TOP,
-          `${routePath.GALLERY}/:id`
-        ]} component={Header} />
-        {/* pathで指定しているURL（トップページ・ギャラリーページ）にルートした場合のみHeaderコンポーネントを表示 */}
-        <Route exact path={routePath.PAGE_TOP} component={Main} />
-        {/* path属性で指定しているURLにアクセスした場合のみMainコンポーネントの内容を表示するようにルートを設定 */}
-        <Route exact path={`${routePath.GALLERY}/:id`} component={List}/>
-        {/* path属性で指定している文字列を含むURL（:idの部分については別コンポーネントで指定）にアクセスした際にListコンポーネントの内容を表示するようにルートを設定 */}
-        <Route path={routePath.LOGIN} component={Login}/>
-        {/* path属性で指定している文字列を含むURLにアクセスした際にLoginコンポーネントの内容を表示するようにルートを設定 */}
-        <Route exact path={routePath.PASSWORD_RESET} component={PasswordReset}/>
-        {/* path属性で指定している文字列を含むURLにアクセスした際にPasswordResetコンポーネントの内容を表示するようにルートを設定 */}
-        </>
+      <Route exact path={routePath.PAGE_DEFAULT}>
+        {/* pathで指定しているURLにアクセスした場合のみに下記を実行 */}
+        <Redirect to={routePath.LOGIN}/>
+        {/* Routeコンポーネントのpathで指定しているURLにアクセスした場合のみ、toで指定したURLにルーティング */}
+      </Route>
+      <Route exact path={[
+        routePath.PAGE_TOP,
+        `${routePath.GALLERY}/:id`
+      ]} component={Header} />
+      {/* pathで指定しているURL（トップページ・ギャラリーページ）にルートした場合のみHeaderコンポーネントを表示 */}
+      <Route exact path={routePath.PAGE_TOP} component={Main} />
+      {/* path属性で指定しているURLにアクセスした場合のみMainコンポーネントの内容を表示するようにルートを設定 */}
+      <Route exact path={`${routePath.GALLERY}/:id`} component={List}/>
+      {/* path属性で指定している文字列を含むURL（:idの部分については別コンポーネントで指定）にアクセスした際にListコンポーネントの内容を表示するようにルートを設定 */}
+      <Route path={routePath.LOGIN} component={Login}/>
+      {/* path属性で指定している文字列を含むURLにアクセスした際にLoginコンポーネントの内容を表示するようにルートを設定 */}
+      <Route exact path={routePath.PASSWORD_RESET} component={PasswordReset}/>
+      {/* path属性で指定している文字列を含むURLにアクセスした際にPasswordResetコンポーネントの内容を表示するようにルートを設定 */}
+    </>
   )
 
 }
--- src/constants/Route.tsx
+++ src/constants/Route.tsx
@@ -1,14 +1,16 @@
 interface RoutePath {
+    PAGE_DEFAULT: string,
     PAGE_TOP: string;
     GALLERY: string;
     LOGIN: string;
     PASSWORD_RESET: string;
 }
-// URLパス用のオブジェクト要素の値の型定義ß
+// URLパス用のオブジェクト要素の値の型定義
 
 
 export const routePath: RoutePath = {
-  PAGE_TOP : "/",
+  PAGE_DEFAULT: "/",
+  PAGE_TOP : "/top",
   GALLERY : "/gallery",
   LOGIN: "/login",
   PASSWORD_RESET: "/password-reset",
