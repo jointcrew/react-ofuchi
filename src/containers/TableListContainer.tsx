098c75b7009f28b45b5fe3445225d5733e88d770
oh-ryo <oh.ryo030205@gmail.com>
2020-03-11 17:19:58 +0900

    refactor: Restoring accidentally deleted code

--- src/containers/TableListContainer.tsx
+++ src/containers/TableListContainer.tsx
@@ -16,7 +16,7 @@ export interface ListData {
   // 文字列型として型定義
   age: number;
   // 数値型として型定義
-  address: string;
+  birthday: string;
   // 文字列型として型定義
   gender: string;
   // 文字列型として型定義
@@ -68,6 +68,25 @@ const TableListContainer: React.FC = ():JSX.Element => {
       // 年齢は数値型のためa.ageからb.ageを引いて0未満であればa.ageをb.ageよりも前の順番にソートし、1以上であればa.ageをb.ageよりも後ろの順番にソートし、0であれば順番のソートを行わない
     },
     {
+      title: "誕生日",
+      dataIndex: "birthday",
+      key: "birthday",
+      sorter: (a, b) :number => {
+        if(a.birthday < b.birthday){
+          // 文字列の記号、数字、アルファベット、ひらがな、カタカナ、漢字の順番で比較した際にa.addressの文字列よりもb.addressの文字列の方が後ろの順番である場合に下記処理を実行
+          return -1;
+          // 返り値として0未満の数値を渡すことによりa.addressをb.addressよりも前の順番にソートする
+        }
+        if(a.birthday > b.birthday){
+          // 文字列の記号、数字、アルファベット、ひらがな、カタカナ、漢字の順番で比較した際にa.addressの文字列よりもb.addressの文字列の方が前の順番である場合に下記処理を実行
+          return 1;
+          // 返り値として1以上の数値を渡すことによりa.addressをb.addressよりも後の順番にソートする
+        }
+        return 0;
+        // 上記if文に当てはまらなかった場合はa.addressとb.addressが同一の値であるため順番のソートを行わない
+      }
+    },
+    {
       title: "性別",
       dataIndex: "gender",
       key: "gender",
@@ -114,7 +133,7 @@ const TableListContainer: React.FC = ():JSX.Element => {
 
   const history = useHistory();
   // hooksのuseHistoryを使用してブラウザヒストリーをhistoryに代入
-  const rowClick = () => history.push(`${routePath.TABLE_LIST}${routePath.DETAIL}`);
+  const rowClick = () => history.push(`${routePath.TABLE_LIST}${routePath.DETAIL}`, TableListData);
   // 特定のアクション時（今回はTableList内の一覧表の各行をクリック）に指定したURLへ遷移するように設定
 
   return(
